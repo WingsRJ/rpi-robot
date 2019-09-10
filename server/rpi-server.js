ae5a0a1615dbac233781bd1fee8fad8ae65010f5
WingsRJ <hongjie940702@gmail.com>
2019-09-11 00:02:16 +0800

    0.01

--- server/rpi-server.js
+++ server/rpi-server.js
@@ -1,1 +1,8 @@
-helloworld//同步测试2
+const express = require("express");
+const app = express();
+app.get("/turn_right", (req, res) => {
+    res.send("turn right");
+});
+app.listen(3000, () => {
+    console.log("Server listening on port 3000");
+});
