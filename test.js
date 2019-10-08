b2cc1f727c8a9a26dfb045bc7d095cdff811ced4
WingsRJ <hongjie940702@gmail.com>
2019-10-08 20:01:14 +0800

    /

--- test.js
+++ test.js
@@ -1,19 +1,12 @@
-const express = require("express");
-const app = express();
-app.use("/Control_Center", express.static("Control_Center"));
-app.use("/TV", express.static("TV"));
-app.use("/libraries", express.static("libraries"));
-app.use("/data", express.static("data"));
+const OMX = require('omx-player-wrapper');
 
-const omx = require('@augmentality/node-omx');
-const n = new omx.Player();
+var omxPlayer = new OMX('data/black.mp4');
 
-n.open('http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_30fps_normal.mp4');
-n.play();
+omxPlayer.start();
 setTimeout(() => {
-    n.stop();
-}, 5000);
+    omxPlayer.pause();
+}, 3000);
 setTimeout(() => {
-    n.open('data/black.mp4');
-    n.play();
-}, 6000);
+    var omxPlayer1 = new OMX('data/RO_wait.mp4');
+    omxPlayer1.start();
+}, 6000)
