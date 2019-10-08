4fc38d23fc3960dfb27c9cfbe4cf9143fa83f33e
WingsRJ <hongjie940702@gmail.com>
2019-10-08 20:16:09 +0800

    /

--- test.js
+++ test.js
@@ -3,10 +3,12 @@ const OMX = require('omx-player-wrapper');
 var omxPlayer = new OMX('data/black.mp4');
 
 omxPlayer.start();
+
 setTimeout(() => {
     omxPlayer.pause();
 }, 3000);
-setTimeout(() => {
-    var omxPlayer1 = new OMX('data/RO_wait.mp4');
-    omxPlayer1.start();
-}, 6000);
+
+// setTimeout(() => {
+//     var omxPlayer1 = new OMX('data/RO_wait.mp4');
+//     omxPlayer1.start();
+// }, 6000);
