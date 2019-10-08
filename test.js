053e006bf5c95091338c499d4c0a0ea9488ff46a
WingsRJ <hongjie940702@gmail.com>
2019-10-08 19:57:18 +0800

    /

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const omx = require("@augmentality/node-omx");
+
+app.use("/Control_Center", express.static("Control_Center"));
+app.use("/TV", express.static("TV"));
+app.use("/libraries", express.static("libraries"));
+app.use("/data", express.static("data"));
+
 const omx = require('@augmentality/node-omx');
 const n = new omx.Player();
 
