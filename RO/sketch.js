6616509a7ead18fa6402d8fbcbd6c1d21b434de9
WingsRJ <hongjie940702@gmail.com>
2019-10-11 18:46:26 +0800

    /

---
+++ RO/sketch.js
@@ -0,0 +1,48 @@
+let videoPlayer;
+let cvs;
+let tag;
+let adress = "192.168.2.7"
+
+var socket;
+
+function preload() {
+    videoPlayer = createVideo("../data/black.mp4");
+    videoPlayer.position(displayWidth, 0);
+    videoPlayer.hide();
+}
+
+function setup() {
+    cvs = createCanvas(windowWidth, windowHeight);
+    textAlign(CENTER, CENTER);
+    textSize(24);
+    fill(255);
+    socket = io.connect("http://" + adress + ":3000");
+    socket.on("RO_action", newAction);
+}
+
+function newAction(_actionName) {
+    console.log("RO_action: " + _actionName);
+    if (_actionName == "All reset") {
+        window.location.reload();
+    } else {
+        tag = _actionName;
+        videoPlayer.attribute("src", "../data/" + _actionName + ".mp4");
+        videoPlayer.play();
+        if (_actionName.match(" ") == " ") {
+
+        }
+    }
+}
+
+function draw() {
+    image(videoPlayer, 0, 0, width, height);
+    text("RO_action: " + tag, width / 2, height / 2);
+}
+
+function keyPressed() {
+    if (keyCode === 13) {
+        let fs = fullscreen();
+        fullscreen(!fs);
+        resizeCanvas(windowWidth, windowHeight);
+    }
+}
--- rpi-server.js
+++ rpi-server.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
-const omx = require('omxdirector');
-
-const TV_position = 60;
-const User_position = -60;
-var Robot_position = 0;
 
 var socket = require("socket.io");
 var server = app.listen(3000);
 var io = socket(server);
+
+const TV_position = 60;
+const User_position = -60;
+var Robot_position = 0;
 
 console.log("Server listening on port 3000");
 
@@ -32,76 +31,41 @@ function newConnection(socket) {
     socket.on("RO_action", RO_action);
 
     function RO_action(_actionName) {
+        socket.broadcast.emit("RO_action", _actionName);
+
         if (_actionName == "RO_wake_up") {
-            playVideo(_actionName);
             //servo
         } else if (_actionName == "RO_here") {
-            playVideo(_actionName);
         } else if (_actionName == "RO_turn_to_user") {
-            console.log("turn to User");
             if (User_position < Robot_position) {
-                playVideo("RO_turn_left",{loop: false});
+                socket.broadcast.emit("RO_action","RO_turn_left");
             } else if (User_position > Robot_position) {
-                playVideo("RO_turn_right",{loop: false});
+                socket.broadcast.emit("RO_action","RO_turn_right");
             }
             //servo
         } else if (_actionName == "RO_turn_to_TV") {
-            console.log("turn  to TV");
             if (TV_position < Robot_position) {
-                playVideo("RO_turn_left",{loop: false});
+                socket.broadcast.emit("RO_action","RO_turn_left");
             } else if (TV_position > Robot_position) {
-                playVideo("RO_turn_right",{loop: false});
+                socket.broadcast.emit("RO_action","RO_turn_right");
             }
             //servo
         } else if (_actionName == "RO_wait") {
-            omx.play("data/RO_wait.mp4",{loop: true});
         } else if (_actionName == "RO_OK") {
-            playVideo(_actionName);
         } else if (_actionName == "RO_sleep") {
-            playVideo(_actionName);
             //servo
         } else if (_actionName == "RO_move_out") {
-            playVideo(_actionName);
         } else if (_actionName == "RO_move_in") {
-            playVideo(_actionName);
         } else if (_actionName == "RO_play(RE)") {
-            playVideo(_actionName);
         } else if (_actionName == "RO_superpower") {
-            playVideo(_actionName);
         } else if (_actionName == "RO_play(CT)") {
-            playVideo(_actionName);
         } else if (_actionName == "RO_communicate") {
-            playVideo(_actionName);
         } else if (_actionName == "RO_play(R)") {
-            playVideo(_actionName);
         } else if (_actionName == "RO_play(CVST)") {
-            playVideo(_actionName);
         } else if (_actionName == "RO_reset") {
-            console.log("reset")
             //servo
-            playVideo("RO_sleep");
-            setTimeout(() => {
-                //servo
-                playVideo("black");
-            }, 3000);
         } else {
            // console.log("Error !");
         }
-    }
-}
-
-function playVideo(_name, wait, delay) {
-    console.log(_name);
-    if(omx.isPlaying()){
-        omx.stop();
-    }
-    omx.play("data/" + _name + ".mp4",{loop: false});
-    if (wait == "w") {
-        setTimeout(() => {
-            if(omx.isPlaying()){
-                omx.stop();
-            }
-            omx.play("data/RO_wait.mp4",{loop: true});
-        }, delay * 1000);
     }
 }
